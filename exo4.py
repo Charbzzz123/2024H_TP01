020f777b0789a91b9e57224083c4d5727d64631b
Charbzzz123 <Charbzzz123@gmail.com>
2024-01-22 16:16:11 -0500

    ex 4 fini

--- exo4.py
+++ exo4.py
@@ -1,3 +1,10 @@
 # TODO : Analyser le message crypté de l'Empire, compter le nombre d'occurrences des voyelles "a", "e", "i", "o", "u" et "y", puis assembler les résultats en une coordonnée galactique.
 
-message = ...
+message = input("Entrez le message de l'Empire :")
+a=message.count('a')
+e=message.count('e')+message.count('E')
+i=message.count('i')
+o=message.count('o')
+u=message.count('u')
+y=message.count('y')
+print('Les coordonnees galactiques sont' ,a,".",e,".",i,".",o,".",u,".",y)
