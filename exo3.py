851d9ea75ab9fcb6e962829e865d7eb882b69810
Charbzzz123 <Charbzzz123@gmail.com>
2024-01-22 16:16:02 -0500

    ex.3 fini

--- exo3.py
+++ exo3.py
@@ -1,4 +1,7 @@
 # TODO : Calculer la distance que chaque rocher lancé par les catapultes peut atteindre en utilisant la formule de la portée d'un projectile.
-
-vitesse_initiale = ...
-angle_lancement = ...
+from math import *
+vitesse_initiale = float(input('Entrez la vitesse initiale (en m/s) :'))
+angle_lancement = float(input("Entrez l'angle de lancement (en degres) :"))
+rad=angle_lancement*pi/180
+g=9.81
+print("La distance parcourue par le projectile est de ",round((vitesse_initiale**2)*sin(2*rad)/g,3),"metres.")
