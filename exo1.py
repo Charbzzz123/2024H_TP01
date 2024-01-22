ef13bcc8d41791193bd5e47774012f5d45a889c1
Charbzzz123 <Charbzzz123@gmail.com>
2024-01-22 18:06:01 -0500

    ex 1 fini

--- exo1.py
+++ exo1.py
@@ -1,5 +1,7 @@
 # TODO : Créer un script Python pour saisir et publier des découvertes planétaires sur un blog intergalactique.
 
 planete = input("Entrez le nom de la planete exploree: ")
-date_visite = ...
-description = ...
+date_visite = input("Entrez la date de visite: ")
+description = input("Entrez la description de la planete: ")
+print("Votre exploration a été ajoutée au Journal Des Astres")
+print("Titre: Decouverte de", planete,"Date: ", date_visite,"Description: ",description)
