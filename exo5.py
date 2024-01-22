50c7df98f058d7003e1f9e3cf7a01f9b3f534df0
Charbzzz123 <Charbzzz123@gmail.com>
2024-01-22 16:16:27 -0500

    ex5 fini

--- exo5.py
+++ exo5.py
@@ -1,3 +1,8 @@
 # TODO : Créer un système d'alerte qui demande le niveau de charge de la batterie, affiche une représentation graphique si la charge est entre 0 et 100%, et affiche un message d'erreur en cas de charge en dehors de la plage normale.
 
-niveau_charge = ...
+niveau_charge = int(input("Entrez le niveau de charge actuel de la batterie :"))
+if 0<=niveau_charge<=100:
+    print(round(niveau_charge/10)*"❚")
+else:
+    print("Erreur : niveau de charge invalide.")
+    
